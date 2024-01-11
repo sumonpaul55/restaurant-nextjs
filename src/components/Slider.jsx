a13f7a40bfeadeebfdf457db64cd4aafd90707da
sumonpaul55 <sumonpaul.web@gamil.com>
2024-01-11 10:05:16 +0600

    padding add to the banner button

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -9,7 +9,7 @@ function Slider() {
                 <div className='flex flex-col md:flex-row'>
                     <div className='flex-1 flex items-start flex-col justify-center font-bold text-xl md:text-2xl gap-6'> {/* text container */}
                         <h1 className='text-orange-600 text-2xl md:text-4xl lg:text-7xl'>test</h1>
-                        <button className='bg-orange-600 text-white px-3 py-1'>Order now</button>
+                        <button className='bg-orange-600 text-white px-3 py-2'>Order now</button>
                     </div>
                     <div className='flex-1 p-3'>
                         <Image src={slide1} alt="slide1" className='rounded-lg' />
